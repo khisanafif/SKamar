377fa47ba58e31047e20922e58f262181ddadf82
Zilva <Zilva@LAPTOP-3USJ4SLR>
2023-06-20 08:33:29 +0700

    Add project files.

---
+++ WinFormsApp3/Kamar2.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WinFormsApp3
+{
+    partial class Kamar2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kamar2));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1264, 681);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Location = new Point(1217, 10);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(35, 40);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.Location = new Point(1153, 13);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(38, 37);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Montserrat", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(519, 496);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 51);
+            button1.TabIndex = 27;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Montserrat Medium", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(406, 372);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 26);
+            label2.TabIndex = 26;
+            label2.Text = "Status Kamar :";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(8, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(67, 37);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Montserrat Medium", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(406, 231);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 26);
+            label1.TabIndex = 25;
+            label1.Text = "Nomor Kamar :";
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Montserrat Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "True", "False" });
+            comboBox2.Location = new Point(624, 367);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(189, 37);
+            comboBox2.TabIndex = 24;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Montserrat Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1", "2", "3" });
+            comboBox1.Location = new Point(624, 226);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(189, 37);
+            comboBox1.TabIndex = 23;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Montserrat", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(510, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(210, 37);
+            label3.TabIndex = 22;
+            label3.Text = "Status Kamar";
+            // 
+            // Kamar2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(panel1);
+            Name = "Kamar2";
+            Text = "Kamar2";
+            Resize += Kamar2_Resize;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp3/Kamar2.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Kamar2 : Form
+    {
+
+        public Kamar2()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            InitializeComponent();
+            
+
+            //NpgsqlConnection conn = new NpgsqlConnection("Server=localhost; Port=5432; Database=Jecation; User Id=postgres; Password=afif2701");
+            //conn.Open();
+            //NpgsqlCommand cmd = new NpgsqlCommand(conn);
+            //cmd.CommandType = CommandType.Text
+            //cmd.CommandType = "UPDATE detail_kamar set status_kamar = @statusKamar where nomor_kamar = {comboBox1.Text}";
+            //    cmd.Parameters.AddWithValue(("@statuskamar"),comboBox2.Text);
+            //    cmd.Parameters.AddWithValue(("@nomorkamar"), comboBox2.Text);
+            //    cmd.ExecuteNonQuery();
+            //    cmd.Dispose();
+            //    conn.Close();
+
+
+        }
+
+        private NpgsqlConnection conn;
+        private NpgsqlCommand cmd;
+        private String conString = String.Format("Server=localhost; Port=5432; Database=Jecation; User Id=postgres; Password=afif2701");
+        private string sql;
+
+        private void Kamar2_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(comboBox1.Text);
+
+            NpgsqlConnection conn = new NpgsqlConnection(conString);
+            {
+                conn.Open();
+                string sql = $"update detail_kamar set status_kamar = {comboBox2.Text} where id_detail_kamar = {comboBox1.Text}";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue(new NpgsqlParameter("@nomor_kamar", comboBox1.Text));
+                cmd.Parameters.AddWithValue(new NpgsqlParameter("@status_kamar", comboBox2.Text));
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("sukses");
+            }
+
+        }
+    }
+}
